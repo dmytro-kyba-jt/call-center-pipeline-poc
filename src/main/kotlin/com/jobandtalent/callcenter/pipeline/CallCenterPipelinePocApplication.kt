53daf89f0f2dfd122fe0868d43016b3ba1b7c2b1
Dmytro Kyba <dmytro.kyba@jobandtalent.com>
2025-05-28 11:17:16 +0300

    Initial commit

---
+++ src/main/kotlin/com/jobandtalent/callcenter/pipeline/CallCenterPipelinePocApplication.kt
@@ -0,0 +1,11 @@
+package com.jobandtalent.callcenter.pipeline
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CallCenterPipelinePocApplication
+
+fun main(args: Array<String>) {
+	runApplication<CallCenterPipelinePocApplication>(*args)
+}
---
+++ src/test/kotlin/com/jobandtalent/callcenter/pipeline/CallCenterPipelinePocApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jobandtalent.callcenter.pipeline
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CallCenterPipelinePocApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
